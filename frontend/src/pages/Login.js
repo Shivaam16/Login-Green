ceac6de2169aa0f5aff19f36ca7eeb153d8e83cb
Shivam <122827835+Shivaam16@users.noreply.github.com>
2025-05-01 13:59:40 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -33,7 +33,7 @@ function Login() {
             return handleError('email and password are required');
         }
         try {
-            const url = `https://deploy-mern-app-api-orpin.vercel.app/auth/login`;
+            const url = `https://login-green-api.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
